aee58799ed1974e67cb0f049f2d3b4a026ab2841
gladlyunicorn <36921343+gladlyunicorn@users.noreply.github.com>
2018-07-04 16:13:06 +0800

    Update sydalg.js

--- sydalg.js
+++ sydalg.js
@@ -9,3 +9,19 @@ $(document).ready(function () {
 	});
 
 	});
+$.get("https://wd.comsci.club/api/kv.php?key=sydalg", function(data) {
+			$("#comments-box").html(data);
+		});
+
+	$("#submitcomment").click(function() {
+		var currentcomments = $("#comments-box").html();
+		var newcomments = currentcomments + "<br>" + "<b>" + $("#commentName").val() + "</b>: " + $("#comment").val();
+		$.post("https://wd.comsci.club/api/kv.php?key=sydalg", {
+			value: newcomments
+		});
+
+		$("#comments-box").html(newcomments);
+
+		$("#commentName").val("");
+		$("#comment").val("");
+	});

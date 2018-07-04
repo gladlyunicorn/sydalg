d1c93f8a2f50e84e6cd6e19aaf363bbdf633b9b3
gladlyunicorn <36921343+gladlyunicorn@users.noreply.github.com>
2018-07-04 15:45:28 +0800

    Update sydalg.js

--- sydalg.js
+++ sydalg.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 
-	$.get("https://wd.comsci.club/api/kv.php?key=myvisitorcount", function(data){
+	$.get("https://wd.comsci.club/api/kv.php?key=sydalg", function(data){
 		var visitorCount = parseInt(data);
 		$("#visitor-count").html(visitorCount);
-		$.post("https://wd.comsci.club/api/kv.php?key=myvisitorcount", {
+		$.post("https://wd.comsci.club/api/kv.php?key=sydalg", {
 		value: visitorCount+1
 	});
 
